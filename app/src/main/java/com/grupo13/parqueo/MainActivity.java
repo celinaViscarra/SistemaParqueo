1a7d5c7cc5bd488310aaf96090305d4284755f7b
Marvin Alvarenga <marvinalexz@gmail.com>
2020-06-14 16:53:21 -0600

    Gestos basicos del mapa

--- app/src/main/java/com/grupo13/parqueo/MainActivity.java
+++ app/src/main/java/com/grupo13/parqueo/MainActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -28,6 +29,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setCompassEnabled(true);
+        uiSettings.setMapToolbarEnabled(true);
+        uiSettings.setZoomGesturesEnabled(true);
+        uiSettings.setScrollGesturesEnabled(true);
+        uiSettings.setTiltGesturesEnabled(true);
+        uiSettings.setRotateGesturesEnabled(true);
+        uiSettings.setZoomControlsEnabled(true);
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng( 13.6929403, -89.2181911);
