5c585041d60a03cd9c991e93a6e985ee91249a86
celinaViscarra <xelaju_celina@yahoo.com>
2020-07-05 15:54:54 -0600

    Agregado de Comments en MainActivity

--- app/src/main/java/com/grupo13/parqueo/MainActivity.java
+++ app/src/main/java/com/grupo13/parqueo/MainActivity.java
@@ -149,6 +149,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMarkerClickListener(this);
 
         UiSettings uiSettings = mMap.getUiSettings();
         uiSettings.setCompassEnabled(true);
@@ -301,7 +302,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
     @Override
     public boolean onMarkerClick(final Marker marker) {
-        Toast.makeText(this,marker.getTitle(),Toast.LENGTH_SHORT).show();
+        //Para desplegar la opcion de comentarios.
+        //Hacer intent.
+        setContentView(R.layout.comments_tool);
+
+        //Toast.makeText(this,marker.getTitle(),Toast.LENGTH_SHORT).show();
         return true;
     }
     public void cargarUbicaciones(){
