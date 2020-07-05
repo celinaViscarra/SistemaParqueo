836598a0da66de87b13233f8fae621a67cb8ec86
celinaViscarra <xelaju_celina@yahoo.com>
2020-07-05 13:08:25 -0600

    Correcion en Comments por cambio de layout.

--- app/src/main/java/com/grupo13/parqueo/comments/Comments.java
+++ app/src/main/java/com/grupo13/parqueo/comments/Comments.java
@@ -30,7 +30,7 @@ public class Comments extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.comments_tool);
 
         initImageBitmas();
     }
